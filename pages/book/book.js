924812569c3c1e0612c2cfd1c8cb9f379cf4eb66
codingRain <congzikk@qq.com>
2019-10-31 20:44:45 +0800

    20191031/11.7~12.1

--- pages/book/book.js
+++ pages/book/book.js
@@ -1,18 +1,29 @@
 // pages/book/book.js
+import {
+  BookModel
+}
+from '../../models/book.js'
+
+const bookModel = new BookModel()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    books: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    bookModel.getHotList().then(res => {
+      this.setData({
+        books: res
+      })
+    })
   },
 
   /**
--- util/http-p.js
+++ util/http-p.js
@@ -1,7 +1,7 @@
 import { config, errorCodes as tips } from '../config.js'
 
 class HTTP {
-  request(url, data={}, method='GET'){
+  request({url, data={}, method='GET'}){
     return new Promise((resolve, reject) => {
       this._request(url, resolve, reject, data, method)
     })
@@ -34,8 +34,12 @@ class HTTP {
   }
 
   _show_error(error_code) {
+    if(!error_code){
+      error_code = 1
+    }
+    const tip = tips[error_code]
     wx.showToast({
-      title: tips[error_code],
+      title: tip ? tip : tips[1],
       icon: 'none',
       duration: 2000
     })
